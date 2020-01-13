424908e822b22ff5240055e1172df4c9d3f54041
Sandvich Thief <jacobholbeck@gmail.com>
2020-01-13 11:59:59 +1000

    Updated Code to use XashXT instead of GoldSrc

--- common/event_api.h
+++ common/event_api.h
@@ -12,40 +12,47 @@
 *   without written permission from Valve LLC.
 *
 ****/
-#if !defined ( EVENT_APIH )
-#define EVENT_APIH
-#ifdef _WIN32
-#pragma once
-#endif
 
-#define EVENT_API_VERSION 1
+#ifndef EVENT_API_H
+#define EVENT_API_H
+
+#define EVENT_API_VERSION	1
 
 typedef struct event_api_s
 {
-	int		version;
-	void	( *EV_PlaySound ) ( int ent, float *origin, int channel, const char *sample, float volume, float attenuation, int fFlags, int pitch );
-	void	( *EV_StopSound ) ( int ent, int channel, const char *sample );
-	int		( *EV_FindModelIndex )( const char *pmodel );
-	int		( *EV_IsLocal ) ( int playernum );
-	int		( *EV_LocalPlayerDucking ) ( void );
-	void	( *EV_LocalPlayerViewheight ) ( float * );
-	void	( *EV_LocalPlayerBounds ) ( int hull, float *mins, float *maxs );
-	int		( *EV_IndexFromTrace) ( struct pmtrace_s *pTrace );
-	struct physent_s *( *EV_GetPhysent ) ( int idx );
-	void	( *EV_SetUpPlayerPrediction ) ( int dopred, int bIncludeLocalClient );
-	void	( *EV_PushPMStates ) ( void );
-	void	( *EV_PopPMStates ) ( void );
-	void	( *EV_SetSolidPlayers ) (int playernum);
-	void	( *EV_SetTraceHull ) ( int hull );
-	void	( *EV_PlayerTrace ) ( float *start, float *end, int traceFlags, int ignore_pe, struct pmtrace_s *tr );
-	void	( *EV_WeaponAnimation ) ( int sequence, int body );
-	unsigned short ( *EV_PrecacheEvent ) ( int type, const char* psz );
-	void	( *EV_PlaybackEvent ) ( int flags, const struct edict_s *pInvoker, unsigned short eventindex, float delay, float *origin, float *angles, float fparam1, float fparam2, int iparam1, int iparam2, int bparam1, int bparam2 );
-	const char *( *EV_TraceTexture ) ( int ground, float *vstart, float *vend );
-	void	( *EV_StopAllSounds ) ( int entnum, int entchannel );
-	void    ( *EV_KillEvents ) ( int entnum, const char *eventname );
+	int	version;
+	void	( *EV_PlaySound )( int ent, float *origin, int channel, const char *sample, float volume, float attenuation, int fFlags, int pitch );
+	void	( *EV_StopSound )( int ent, int channel, const char *sample );
+	int	( *EV_FindModelIndex )( const char *pmodel );
+	int	( *EV_IsLocal )( int playernum );
+	int	( *EV_LocalPlayerDucking )( void );
+	void	( *EV_LocalPlayerViewheight )( float * );
+	void	( *EV_LocalPlayerBounds )( int hull, float *mins, float *maxs );
+	int	( *EV_IndexFromTrace)( struct pmtrace_s *pTrace );
+	struct physent_s *( *EV_GetPhysent )( int idx );
+	void	( *EV_SetUpPlayerPrediction )( int dopred, int bIncludeLocalClient );
+	void	( *EV_PushPMStates )( void );
+	void	( *EV_PopPMStates )( void );
+	void	( *EV_SetSolidPlayers )( int playernum );
+	void	( *EV_SetTraceHull )( int hull );
+	void	( *EV_PlayerTrace )( float *start, float *end, int traceFlags, int ignore_pe, struct pmtrace_s *tr );
+	void	( *EV_WeaponAnimation )( int sequence, int body );
+	unsigned short ( *EV_PrecacheEvent )( int type, const char* psz );
+	void	( *EV_PlaybackEvent )( int flags, const struct edict_s *pInvoker, unsigned short eventindex, float delay, float *origin, float *angles, float fparam1, float fparam2, int iparam1, int iparam2, int bparam1, int bparam2 );
+	const char *( *EV_TraceTexture )( int ground, float *vstart, float *vend );
+	void	( *EV_StopAllSounds )( int entnum, int entchannel );
+	void	( *EV_KillEvents )( int entnum, const char *eventname );
+
+	// Xash3D extension
+	void	( *EV_PlayerTraceExt )( float *start, float *end, int traceFlags, int (*pfnIgnore)( struct physent_s *pe ), struct pmtrace_s *tr );
+	const char *(*EV_SoundForIndex)( int index );
+	struct msurface_s *( *EV_TraceSurface )( int ground, float *vstart, float *vend );
+	struct movevars_s *( *EV_GetMovevars )( void );
+	struct pmtrace_s *( *EV_VisTraceLine )( float *start, float *end, int flags );
+	struct physent_s *( *EV_GetVisent )( int idx );
+	int	( *EV_TestLine)( const vec3_t start, const vec3_t end, int flags );
+	void	( *EV_PushTraceBounds)( int hullnum, const float *mins, const float *maxs );
+	void	( *EV_PopTraceBounds)( void );
 } event_api_t;
 
-extern event_api_t eventapi;
-
-#endif
+#endif//EVENT_API_H
--- common/ref_params.h
+++ common/ref_params.h
@@ -12,23 +12,24 @@
 *   without written permission from Valve LLC.
 *
 ****/
-#if !defined( REF_PARAMSH )
-#define REF_PARAMSH
+
+#ifndef REF_PARAMS_H
+#define REF_PARAMS_H
 
 typedef struct ref_params_s
 {
-	// Output
-	float	vieworg[3];
-	float	viewangles[3];
+	// output
+	vec3_t		vieworg;
+	vec3_t		viewangles;
 
-	float	forward[3];
-	float	right[3];
-	float   up[3];
-
+	vec3_t		forward;
+	vec3_t		right;
+	vec3_t		up;
+	
 	// Client frametime;
-	float	frametime;
+	float		frametime;
 	// Client time
-	float	time;
+	float		time;
 
 	// Misc
 	int		intermission;
@@ -37,39 +38,68 @@ typedef struct ref_params_s
 	int		onground;
 	int		waterlevel;
 
-	float	simvel[3];
-	float	simorg[3];
+	vec3_t		simvel;
+	vec3_t		simorg;
 
-	float	viewheight[3];
-	float	idealpitch;
+	vec3_t		viewheight;
+	float		idealpitch;
 
-	float	cl_viewangles[3];
-
+	vec3_t		cl_viewangles;
 	int		health;
-	float	crosshairangle[3];
-	float	viewsize;
+	vec3_t		crosshairangle;
+	float		viewsize;
 
-	float	punchangle[3];
+	vec3_t		punchangle;
 	int		maxclients;
 	int		viewentity;
 	int		playernum;
 	int		max_entities;
-	int		demoplayback;
+	int		demoplayback;	
 	int		hardware;
-
 	int		smoothing;
 
 	// Last issued usercmd
-	struct usercmd_s *cmd;
+	struct usercmd_s	*cmd;
 
 	// Movevars
-	struct movevars_s *movevars;
+	struct movevars_s	*movevars;
 
-	int		viewport[4];		// the viewport coordinates x ,y , width, height
-
-	int		nextView;			// the renderer calls ClientDLL_CalcRefdef() and Renderview
-								// so long in cycles until this value is 0 (multiple views)
-	int		onlyClientDraw;		// if !=0 nothing is drawn by the engine except clientDraw functions
+	int		viewport[4];	// the viewport coordinates x, y, width, height
+	int		nextView;		// the renderer calls ClientDLL_CalcRefdef() and Renderview
+					// so long in cycles until this value is 0 (multiple views)
+	int		onlyClientDraw;	// if !=0 nothing is drawn by the engine except clientDraw functions
 } ref_params_t;
 
-#endif // !REF_PARAMSH
+// same as ref_params but for overview mode
+typedef struct ref_overview_s
+{
+	vec3_t		origin;
+	qboolean		rotated;
+
+	float		xLeft;
+	float		xRight;
+	float		yTop;
+	float		yBottom;
+	float		zFar;
+	float		zNear;
+	float		flZoom;
+} ref_overview_t;
+
+// ref_viewpass_t->flags
+#define RF_DRAW_WORLD	(1<<0)		// pass should draw the world (otherwise it's player menu model)
+#define RF_DRAW_CUBEMAP	(1<<1)		// special 6x pass to render cubemap\skybox sides
+#define RF_DRAW_OVERVIEW	(1<<2)		// overview mode is active
+#define RF_ONLY_CLIENTDRAW	(1<<3)		// nothing is drawn by the engine except clientDraw functions
+
+// intermediate struct for viewpass (or just a single frame)
+typedef struct ref_viewpass_s
+{
+	int		viewport[4];	// size of new viewport
+	vec3_t		vieworigin;	// view origin
+	vec3_t		viewangles;	// view angles
+	int		viewentity;	// entitynum (P2: Savior uses this)
+	float		fov_x, fov_y;	// vertical & horizontal FOV
+	int		flags;		// if !=0 nothing is drawn by the engine except clientDraw functions
+} ref_viewpass_t;
+
+#endif//REF_PARAMS_H
